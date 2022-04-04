9b41339dfd4faa1cdf5293f27acab6f24d57422e
RamavathyNidamanuri <ramacse9@gmail.com>
2022-04-04 13:46:35 -0400

    WebSockets-Chat-with-SocketIO

--- index.js
+++ index.js
@@ -10,6 +10,29 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+      console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+  });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
